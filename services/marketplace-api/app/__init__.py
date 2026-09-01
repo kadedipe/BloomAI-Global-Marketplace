1a9ab500b92deda31603f477a06cfa864bc9e20d
Dr. KOLAPO ADEDIPE <57101314+kadedipe@users.noreply.github.com>
2026-09-01 19:44:16 +0300

    Register support case routes

--- services/marketplace-api/app/__init__.py
+++ services/marketplace-api/app/__init__.py
@@ -1,5 +1,5 @@
 from .notifications import api_router
-from . import admin_refunds, commerce, hardening_routes, production_validation, support_assistant
+from . import admin_refunds, commerce, hardening_routes, production_validation, support_assistant, support_cases
 from . import hardening_compat as hardening_compat
 
 _existing_routes = list(api_router.routes)
@@ -8,5 +8,6 @@ api_router.include_router(hardening_routes.router)
 api_router.include_router(production_validation.router)
 api_router.include_router(admin_refunds.router)
 api_router.include_router(support_assistant.router)
+api_router.include_router(support_cases.router)
 api_router.routes.extend(_existing_routes)
 api_router.include_router(commerce.router)
